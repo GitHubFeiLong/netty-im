a6452b614dda9dc666ff3975ca656548cc173c52
GiteeFeilong <1696741038@qq.com>
2026-04-14 12:21:23 +0800

    登录和退出

--- src/main/java/com/feilong/im/controller/LoginRegisterController.java
+++ src/main/java/com/feilong/im/controller/LoginRegisterController.java
@@ -13,10 +13,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -24,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @Tag(name = "登录注册")
 @Slf4j
+@RequestMapping("/auth")
 @RestController
 @RequiredArgsConstructor
 public class LoginRegisterController {
